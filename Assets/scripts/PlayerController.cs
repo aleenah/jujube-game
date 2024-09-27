cf09bb5bf0c580469a753bf04369442f5c5e4f96
Aleenah Alam <aleenah0909@gmail.com>
2024-09-27 19:58:55 -0400

    finishing game

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,25 +7,55 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 move;
-    bool jump = false;
 
+    bool jump = false;
     int score = 0;
+
     public TMP_Text scoreText;
+    public TMP_Text timeText;
+    public float timeLeft = 60;
+
+    Vector3 startPOS;
+
+    public LayerMask ground;
+
+    AudioSource src;
+    public AudioClip pointSound;
+    public AudioClip jumpSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPOS = transform.position;
+        src = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < -9)
+        {
+            transform.position = startPOS;
+        }
+
+       timeLeft -= Time.deltaTime;
+       if (timeLeft <= 0)
+       {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lose");
+       }
+       timeText.text = "Time: " + timeLeft.ToString("0.0");
+       
+       var feet = new Vector2(transform.position.x, transform.position.y - 0.7f);
+       var dimensions = new Vector2(0.8f, 0.1f);
+       var grounded = Physics2D.OverlapBox(feet, dimensions, 0, ground);
+
        move.x = Input.GetAxis("Horizontal");
 
-       if (Input.GetButtonDown("Jump")) 
+       if (Input.GetButtonDown("Jump") && grounded) 
        {
         jump = true;
+        src.PlayOneShot(jumpSound);
        }
 
     }
@@ -48,6 +78,7 @@ public class PlayerController : MonoBehaviour
             score++;
             scoreText.text = "Score: " + score.ToString();
             Destroy(collision.gameObject);
+            src.PlayOneShot(pointSound);
         }
     }
 }
